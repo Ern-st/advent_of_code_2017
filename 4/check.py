34c72560de9effba1310a9c97244544961218942
Jeppe Ernst <impala.lowrider@gmail.com>
2017-12-04 20:45:04 +0100

    4 part 2

--- 4/check.py
+++ 4/check.py
@@ -5,10 +5,20 @@ input = f.readlines()
 passwordCandidates = [line.rstrip().split(" ") for line in input]
 validPasswords = len(passwordCandidates)
 
-print validPasswords
+print "part 1: candidates " + str(validPasswords)
 
 for password in passwordCandidates:
 	if len(password) != len(set(password)):
 		validPasswords -= 1
 
-print validPasswords
+print "part 1: valid " + str(validPasswords)
+
+validPasswords = len(passwordCandidates)
+print "part 2: candidates " + str(validPasswords)
+
+for password in passwordCandidates:
+	sortedPassword = [''.join(sorted(word)) for word in password]
+	if len(sortedPassword) != len(set(sortedPassword)):
+		validPasswords -= 1
+
+print "part 2: valid " + str(validPasswords)
