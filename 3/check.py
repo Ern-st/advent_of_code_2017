8f34d73598235d0f5a56957f00a27fd33534ad9f
Jeppe Ernst <impala.lowrider@gmail.com>
2017-12-05 18:50:37 +0100

    3 part 2

--- 3/check.py
+++ 3/check.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+from decimal import *
 f = open("input.txt")
 input = f.readlines()
 f.close()
 
-lines = [sorted([int(s) for s in line.rstrip().split("\t")]) for line in input]
+lines = [sorted([Decimal(s) for s in line.rstrip().split("\t")]) for line in input]
 checksum = 0
 
 for numberSet in lines:
-	checksum += (numberSet[-1] - numberSet[0])
+	for number in numberSet:
+		for modulo in numberSet:
+			if (number / modulo) % 1 == 0 and (number / modulo) != 1:
+				print "{0} / {1} = {2}".format(number, modulo, number / modulo)
+				checksum += (number / modulo)
 
-print checksum
+print "checksum: " + str(checksum)
