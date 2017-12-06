56a1383e60cac943b2fcf40ece81d79b815c9c24
Jeppe Ernst <impala.lowrider@gmail.com>
2017-12-06 22:37:07 +0100

    3 part 1

---
+++ 3/check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+input = 368078
+
+boxes = [1]
+increment = 1
+
+while boxes[-1] < input:
+	increment += 2
+	boxes.append(pow(increment, 2))
+
+lastBox = range(boxes[-2] + 1, boxes[-1] + 1)
+sideLength = (boxes[-1] - boxes[-2]) / 4
+
+side = boxes[-1]
+
+while side > input:
+	side -= sideLength
+
+distance = abs((side + (sideLength/2)) - input )
+
+print "distance = {0}".format( distance + len(boxes) - 1)
