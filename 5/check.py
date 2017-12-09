19496a4b77d07ca213264aca1d22484f69098407
Jeppe Ernst <impala.lowrider@gmail.com>
2017-12-09 20:30:52 +0100

    5 part 2

--- 5/check.py
+++ 5/check.py
@@ -2,11 +2,28 @@
 f = open("input.txt")
 input = [int(jump.rstrip()) for jump in f.readlines()]
 
-moves = 0
-index = 0
-while index >= 0 and index < len(input):
-	value = input[index]
-	input[index] = input[index] + 1
-	index = index + value
-	moves += 1
-print moves
+def solvePartOne(inputList):
+	moves = 0
+	index = 0
+	while index >= 0 and index < len(inputList):
+		value = inputList[index]
+		inputList[index] = inputList[index] + 1
+		index = index + value
+		moves += 1
+	print "solution part 1: {}".format(moves)
+
+def solvePartTwo(inputList):
+	moves = 0
+	index = 0
+	while index >= 0 and index < len(inputList):
+		value = inputList[index]
+		if inputList[index] >= 3:
+			inputList[index] -= 1
+		else:
+			inputList[index] += 1
+		index += value
+		moves += 1
+	print "solution part 2: {}".format(moves)
+
+solvePartOne(input[:])
+solvePartTwo(input[:])
